3aaa77084152c921672134923fbda99336086256
Fares-sharabati <106887699+Fares-sharabati@users.noreply.github.com>
2026-09-10 15:47:10 +0300

    Load organizer styles globally on direct refresh

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './lightsync-mobile.css'
+import './styles/lightsync.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
