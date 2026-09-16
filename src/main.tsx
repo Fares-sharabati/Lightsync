cb5479f866959a28fe950b1333e9348a9929f3fd
Fares-sharabati <106887699+Fares-sharabati@users.noreply.github.com>
2026-09-16 14:06:39 +0300

    Load professional UI layer

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import './lightsync-mobile.css'
 import './styles/lightsync.css'
 import './styles/theme.css'
+import './styles/pro-ui.css'
 import App from './App.tsx'
 import { LanguageProvider } from './i18n/LanguageContext'
 
