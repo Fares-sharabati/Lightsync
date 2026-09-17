b361faaf99d2f241273af6315aa40cf80d0df3e8
Fares-sharabati <106887699+Fares-sharabati@users.noreply.github.com>
2026-09-17 09:20:02 +0300

    Import mobile audience polish stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './styles/lightsync.css'
 import './styles/theme.css'
 import './styles/pro-ui.css'
 import './styles/sports-pro.css'
+import './styles/mobile-pro.css'
 import App from './App.tsx'
 import { LanguageProvider } from './i18n/LanguageContext'
 
